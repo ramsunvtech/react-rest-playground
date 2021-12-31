4b610ee476783aee2acfa670a9be8050fcd17a41
yuvaraj <yuvarajravi2@gmail.com>
2021-12-31 17:47:04 +0530

    feat(tabs) Add tabs component (#7)

--- src/components/blocks/InputParams/InputParams.js
+++ src/components/blocks/InputParams/InputParams.js
@@ -2,26 +2,27 @@ import React from 'react'
 import BodyParams from '@/blocks/InputParams/BodyParams'
 import HeaderParams from '@/blocks/InputParams/HeaderParams'
 import QueryParams from '@/blocks/InputParams/QueryParams'
+import Tabs from '@/ui/common/Tabs'
 
 function InputParams(props) {
   const { register, control } = props
 
   return (
-    <>
-      <div>Query Params</div>
-      <QueryParams register={register} control={control} />
-      <br />
-
-      <div>Header Params</div>
-      <HeaderParams register={register} control={control} />
-      <br />
-
-      <div>Body Params</div>
-      <BodyParams register={register} control={control} />
-      <br />
-    </>
+    <Tabs>
+      <Tabs.Item id="query" title="Query Params">
+        <p>Query Params</p>
+        <QueryParams register={register} control={control} />
+      </Tabs.Item>
+      <Tabs.Item id="header" title="Headers">
+        <p>Header Params</p>
+        <HeaderParams register={register} control={control} />
+      </Tabs.Item>
+      <Tabs.Item id="body" title="Body">
+        <p>Body Params</p>
+        <BodyParams register={register} control={control} />
+      </Tabs.Item>
+    </Tabs>
   )
-
 }
 
 export default InputParams
