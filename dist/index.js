3e17b5c598c02aafaf515e29d8895839dfc49282
Venkat.R <ramsunvtech@gmail.com>
2022-01-03 20:51:21 +0800

    Release v.0.1.12

--- dist/index.js
+++ dist/index.js
@@ -19,6 +19,8 @@ var _useFetchApi = _interopRequireDefault(require("./hooks/useFetchApi"));
 
 var _convertParams = _interopRequireDefault(require("./utils/convert-params"));
 
+require("./styles/main.css");
+
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 function _getRequireWildcardCache(nodeInterop) { if (typeof WeakMap !== "function") return null; var cacheBabelInterop = new WeakMap(); var cacheNodeInterop = new WeakMap(); return (_getRequireWildcardCache = function (nodeInterop) { return nodeInterop ? cacheNodeInterop : cacheBabelInterop; })(nodeInterop); }
